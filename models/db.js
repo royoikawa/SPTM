c3041ae95ba5daa19c7aea0e757cce3dc1582b09
Roy Liao <royliao88@gmail.com>
2025-02-10 23:56:57 +0800

    更改 伺服器上DB資料庫是小寫

--- models/db.js
+++ models/db.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'SPTMDB',
+  database: 'sptmdb',
   password: 'wskonoha',
   port: 5432, // PostgreSQL 預設 port
 });
